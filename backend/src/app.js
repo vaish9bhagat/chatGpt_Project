e974eb288fbdc201c4906363a302eae131441065
vaishnav bhagat <bhagatvaishnav07@gmail.com>
2025-11-03 12:20:47 +0530

    deploy1

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,15 +3,21 @@ const authRoutes = require("./routes/authroutes");
 const chatRoutes = require("./routes/chatroutes");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../public")))
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
 }))
 app.use("/auth", authRoutes);
 app.use("/", chatRoutes);
+
+app.get("*name", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
 
 module.exports = app;
